88a3816d5a109a4ab2cd3b944f849b4e9ba962c8
KATARI REVATHI <katari.revathi@gmail.com>
2024-06-21 11:27:19 +0530

    message

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
 # Ensure pip is available
-/usr/bin/python3.9 -m ensurepip --upgrade
+#/usr/bin/python3.9 -m ensurepip --upgrade
 
 # Upgrade pip
-/usr/bin/python3.9 -m pip install --upgrade pip
+#/usr/bin/python3.9 -m pip install --upgrade pip
 
 # Install dependencies from requirements.txt
-/usr/bin/python3.9 -m pip install -r requirements.txt
+#/usr/bin/python3.9 -m pip install -r requirements.txt
+
+# Collect static files
+#python3.9 manage.py collectstatic --noinput
+
+
+# Apply database migrations
+#python3.9 manage.py makemigrations
+#python3.9 manage.py migrate
+#!/bin/bash
+
+# Ensure pip is available
+python3.9 -m ensurepip --upgrade
+
+# Upgrade pip
+python3.9 -m pip install --upgrade pip
+
+# Install dependencies from requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Collect static files
 python3.9 manage.py collectstatic --noinput
 
-
 # Apply database migrations
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
+
